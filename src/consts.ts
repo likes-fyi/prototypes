b732fd65c72e9f5eb648f4b1ae07340308b8e542
Ben Bleything <ben@bleything.net>
2026-04-29 12:36:57 -0700

    update references to miniblog in source

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Miniblog";
-export const SITE_DESCRIPTION =
-  "An opinionated and minimal Astro blog template.";
-export const SITE_URL = "https://miniblog.nicholasly.com/";
+export const SITE_TITLE = "likes.fyi prototypes";
+export const SITE_DESCRIPTION = "my prototypes. let me show you them.";
+export const SITE_URL = "https://prototypes.likes.fyi";
