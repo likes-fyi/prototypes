838da9e2a339f70f175379c7763c94bee10761e5
Ben Bleything <ben@bleything.net>
2026-04-29 20:17:58 -0700

    rss feed gets title and description from root index.mdx

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,12 @@
 import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
-import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
+import { frontmatter } from "./index.mdx"
 
 export async function GET(context) {
   const posts = await getCollection("blog");
   return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
+    title: frontmatter.title,
+    description: frontmatter.subtitle,
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
